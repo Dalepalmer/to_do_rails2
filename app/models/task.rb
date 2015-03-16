1c92efd9e826beb2e7319cbca8dd48b6abac1099
Dale <dap38@zips.uakron.edu>
2015-03-16 16:46:22 -0700

    Made an app

---
+++ app/models/task.rb
@@ -0,0 +1,9 @@
+class Task < ActiveRecord::Base
+  belongs_to :list
+
+  scope :done, -> { where(done: true) }
+  scope :not_done, -> { where(done: false) }
+  validates :description, :presence => true
+
+
+end
